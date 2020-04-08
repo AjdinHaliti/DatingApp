5ac5928e9abc9f510ea3251e3a8c66c39bb3b028
Ajdin Haliti <ajdin14798@gmail.com>
2020-04-08 18:22:17 +0200

    Working on security. Adding Login and Registerserver side.

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {
 
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class ValuesController : ControllerBase // controllerBase gives acces to HTTP responses and Actions(IActionResault...)
     {
         private readonly DataContext _context;
         public ValuesController(DataContext context)
@@ -21,6 +23,7 @@ namespace DatingApp.API.Controllers
         }
 
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
