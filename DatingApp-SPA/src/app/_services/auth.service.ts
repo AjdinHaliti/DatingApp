a1bf820756b4220ad38c54b9e2bae93a0cd238cc
Ajdin Haliti <ajdin14798@gmail.com>
2020-04-11 17:41:19 +0200

    Navbar

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {map} from 'rxjs/operators'
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  baseUrl='http://localhost:5000/api/auth/';
+  baseUrl = 'http://localhost:5000/api/auth/';
 
 constructor(private http: HttpClient) { }
 
